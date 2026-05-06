663231ad28acd1475befa5e055d3b30ed07667bb
Enmanuel Balcacer <balcacer.enmanuel@gmail.com>
2026-05-06 14:39:36 -0400

    feat: update app icons and configuration for improved branding and environment handling

--- mobile/src/shared/constants/config.ts
+++ mobile/src/shared/constants/config.ts
@@ -8,5 +8,7 @@ export const API_PORT = 4000;
 export const API_BASE_URL = __DEV__
   ? Platform.OS === 'android'
     ? `http://${ANDROID_EMULATOR_HOST}:${API_PORT}`
-    : `http://localhost:${API_PORT}`
-  : 'https://api.greenloop.do';
+  : Platform.OS === 'ios'
+    ? 'https://xkvglcrj-4000.use2.devtunnels.ms'
+    : `http://localhost:${API_PORT}` // For web, we assume the backend is proxied at the same origin.
+  : 'api.greenloop.do'; // Production URL
